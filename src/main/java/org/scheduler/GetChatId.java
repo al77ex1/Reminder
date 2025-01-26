16380ac54bb1b4f3abfc215ede6d7df1c69b5699
Alexey Shatunov <al77ex1@gmail.com>
2025-01-26 20:36:32 +0300

    added group chat id

---
+++ src/main/java/org/scheduler/GetChatId.java
@@ -0,0 +1,17 @@
+package org.scheduler;
+
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+public class GetChatId {
+    public static void main(String[] args) {
+        try {
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            botsApi.registerBot(new TelegramBot());
+            System.out.println("Bot started! Send a message to the group to get CHAT_ID");
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
