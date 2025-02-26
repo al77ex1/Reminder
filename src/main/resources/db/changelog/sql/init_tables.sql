39b5161795d5a41f3426c080f7ab4fb04cf91bb3
Alexey Shatunov <al77ex1@gmail.com>
2025-02-26 21:20:11 +0300

    modified table fields

--- src/main/resources/db/changelog/sql/init_tables.sql
+++ src/main/resources/db/changelog/sql/init_tables.sql
@@ -1,6 +1,9 @@
 -- Create notification table
 CREATE TABLE IF NOT EXISTS notification (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    message VARCHAR(500) NOT NULL,
+    job_name VARCHAR(50) NOT NULL,
+    trigger_name VARCHAR(50) NOT NULL,
+    cron_schedule VARCHAR(20) NOT NULL,
+    message TEXT NOT NULL,
     created_at TIMESTAMP NOT NULL
 );
