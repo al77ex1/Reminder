840146af0681264bc0300cd4f7f7f643aa6affe4
Alexey Shatunov <al77ex1@gmail.com>
2025-02-27 22:57:54 +0300

    added support credential from env

--- src/main/java/org/scheduler/TelegramMessageScheduler.java
+++ src/main/java/org/scheduler/TelegramMessageScheduler.java
@@ -1,5 +1,6 @@
 package org.scheduler;
 
+import org.scheduler.config.EnvConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TelegramMessageScheduler {
 
     public static void main(String[] args) {
+        EnvConfig.loadEnvVariables();
         SpringApplication.run(TelegramMessageScheduler.class, args);
     }
 }
---
+++ src/main/java/org/scheduler/config/EnvConfig.java
@@ -0,0 +1,19 @@
+package org.scheduler.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.DotenvEntry;
+
+import java.util.Set;
+
+public class EnvConfig {
+    private EnvConfig() {}
+
+    public static void loadEnvVariables() {
+        Dotenv dotenv = Dotenv.load();
+        Set<DotenvEntry> entries = dotenv.entries();
+
+        for (DotenvEntry entry : entries) {
+            System.setProperty(entry.getKey(), entry.getValue());
+        }
+    }
+}
