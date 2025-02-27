864ffcfde2954fdb7f6e92ef1dfa7b092922d8d7
Alexey Shatunov <al77ex1@gmail.com>
2025-02-27 18:50:47 +0300

    removed sqlite

--- src/main/resources/db/changelog/sql/init_tables.sql
+++ src/main/resources/db/changelog/sql/init_tables.sql
@@ -1,9 +1,9 @@
 -- Create notification table
-CREATE TABLE IF NOT EXISTS notification (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    job_name VARCHAR(50) NOT NULL,
-    trigger_name VARCHAR(50) NOT NULL,
-    cron_schedule VARCHAR(20) NOT NULL,
-    message TEXT NOT NULL,
-    created_at TIMESTAMP NOT NULL
+CREATE TABLE notification (
+    id              SERIAL PRIMARY KEY,
+    job_name        VARCHAR(50) NOT NULL,
+    trigger_name    VARCHAR(50) NOT NULL,
+    cron_schedule   VARCHAR(20) NOT NULL,
+    message TEXT    NOT NULL,
+    created_at      TIMESTAMP WITHOUT TIME ZONE
 );
